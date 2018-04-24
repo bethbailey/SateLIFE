6eeb2388387b141c8f181e9e8b297979aaf95ef3
Jo Denby <jgdenby@uchicago.edu>
2018-04-24 17:06:13 -0500

    variable name cleanup

--- geeimage.js
+++ geeimage.js
@@ -8,21 +8,21 @@ var LSTvisParams = {bands: ['LST_Day_1km'], min:14950, max:15500};
 
 
 // temporal filtering
-var y_2014 = LSTimageCollection.filterDate('2014-01-01', '2014-12-31').median();
-var y_2013 = LSTimageCollection.filterDate('2013-01-01', '2013-12-31').median();
-var y_2012 = LSTimageCollection.filterDate('2012-01-01', '2012-12-31').median();
-var y_2011 = LSTimageCollection.filterDate('2011-01-01', '2011-12-31').median();
-var y_2010 = LSTimageCollection.filterDate('2010-01-01', '2010-12-31').median();
-var y_2009 = LSTimageCollection.filterDate('2009-01-01', '2009-12-31').median();
-var y_2008 = LSTimageCollection.filterDate('2008-01-01', '2008-12-31').median();
-var y_2007 = LSTimageCollection.filterDate('2007-01-01', '2007-12-31').median();
-var y_2006 = LSTimageCollection.filterDate('2006-01-01', '2016-12-31').median();
-var y_2005 = LSTimageCollection.filterDate('2005-01-01', '2005-12-31').median();
-var y_2004 = LSTimageCollection.filterDate('2004-01-01', '2004-12-31').median();
-var y_2003 = LSTimageCollection.filterDate('2003-01-01', '2003-12-31').median();
-var y_2002 = LSTimageCollection.filterDate('2002-01-01', '2002-12-31').median();
-var y_2001 = LSTimageCollection.filterDate('2001-01-01', '2001-12-31').median();
-var y_2000 = LSTimageCollection.filterDate('2000-01-01', '2000-12-31').median();
+var LST_2014 = LSTimageCollection.filterDate('2014-01-01', '2014-12-31').median();
+var LST_2013 = LSTimageCollection.filterDate('2013-01-01', '2013-12-31').median();
+var LST_2012 = LSTimageCollection.filterDate('2012-01-01', '2012-12-31').median();
+var LST_2011 = LSTimageCollection.filterDate('2011-01-01', '2011-12-31').median();
+var LST_2010 = LSTimageCollection.filterDate('2010-01-01', '2010-12-31').median();
+var LST_2009 = LSTimageCollection.filterDate('2009-01-01', '2009-12-31').median();
+var LST_2008 = LSTimageCollection.filterDate('2008-01-01', '2008-12-31').median();
+var LST_2007 = LSTimageCollection.filterDate('2007-01-01', '2007-12-31').median();
+var LST_2006 = LSTimageCollection.filterDate('2006-01-01', '2016-12-31').median();
+var LST_2005 = LSTimageCollection.filterDate('2005-01-01', '2005-12-31').median();
+var LST_2004 = LSTimageCollection.filterDate('2004-01-01', '2004-12-31').median();
+var LST_2003 = LSTimageCollection.filterDate('2003-01-01', '2003-12-31').median();
+var LST_2002 = LSTimageCollection.filterDate('2002-01-01', '2002-12-31').median();
+var LST_2001 = LSTimageCollection.filterDate('2001-01-01', '2001-12-31').median();
+var LST_2000 = LSTimageCollection.filterDate('2000-01-01', '2000-12-31').median();
 
 
 // add layers
