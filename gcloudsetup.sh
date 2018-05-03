8f31b1d0805ef6ad3e94d312c1c0e487ef779b3c
Jo Denby <jgdenby@uchicago.edu>
2018-05-03 18:06:15 -0500

    fixing dependencies installation

--- gcloudsetup.sh
+++ gcloudsetup.sh
@@ -8,47 +8,40 @@ else
 		gcloud compute scp ~/.ssh/google-cloud-cs123 earth-$i:~/.ssh/id_rsa --ssh-key-file=~/.ssh/google-cloud-cs123
 	done
 	gcloud compute instances list --format=text \
-    | grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g' >> intIPs
-    for i in `seq 1 $1`; do
-    	gcloud compute scp intIPs earth-$i:~/ --ssh-key-file=~/.ssh/google-cloud-cs123
-    	gcloud compute ssh earth-$i --ssh-key-file=~/.ssh/google-cloud-cs123
-    	cat intIPs >> hosts
-    	rm intIPs
-    	echo "Installing dependencies on earth-$i..."
-    	sudo apt-get -y install mpich
-    	sudo apt-get -y install python-pip
-    	sudo apt-get -y install python-dev
-    	yes | sudo pip install mpi4py
-    	sudo apt-get -y install python3-dev
-    	sudo apt-get -y install python3-pip
-    	yes | sudo pip3 install mpi4py
-		yes | sudo pip install numpy
-		yes | sudo pip3 install numpy
-		logout
+	| grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g' >> intIPs
+	for i in `seq 1 $1`; do
+		gcloud compute scp intIPs earth-$i:~/ --ssh-key-file=~/.ssh/google-cloud-cs123
+		gcloud compute ssh earth-$i --ssh-key-file=~/.ssh/google-cloud-cs123 --compute="cat intIPs >> hosts; 
+		cat intIPs >> hosts; 
+		rm intIPs; 
+		echo 'Installing dependencies on earth-$i...'; 
+		sudo apt-get -y install mpich; 
+		sudo apt-get -y install python-pip; 
+		sudo apt-get -y install python-dev; 
+		yes | sudo pip install mpi4py; 
+		sudo apt-get -y install python3-dev; 
+		sudo apt-get -y install python3-pip; 
+		yes | sudo pip3 install mpi4py; 
+		yes | sudo pip install numpy; 
+		yes | sudo pip3 install numpy"
+
+
+		# Below is probably wrong
 		for i in `seq 1 $(($1-1))`; do
 			gcloud compute ssh earth-$i --ssh-key-file=~/.ssh/google-cloud-cs123
 			echo "SSHing from earth-$i..."
 			for j in `seq $(($i+1)) $1`; do
 				echo "...to earth-$j"
 				gcloud compute ssh earth-$j --ssh-key-file=~/google-cloud-cs123
+				gcloud compute ssh earth-$i --ssh-key-file=~/google-cloud-cs123
 			done
 		done
-    done
+	done
 fi
-
-# TO EXTRACT INTERNAL IPs 
-#gcloud compute instances list --format=text \
-#    | grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g'
 
 
 # TO DELETE VM INSTANCES
 #gcloud compute instances delete my-instance --zone us-central1-a
-
-
-# TO COPY SOMETHING OVER TO VM INSTANCES (combination of two below)
-#scp -i .ssh/google-cloud-cs123 .ssh/google-cloud-cs123 username@external-IP-1:~/.ssh/id_rsa
-# gcloud compute scp ~/file-1 my-instance:~/remote-destination --zone us-central1-a
-
 
 # TODO
 #
