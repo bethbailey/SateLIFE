91d2c1d23456cf14562373fdee5d003f66be6940
Jo Denby <jgdenby@uchicago.edu>
2018-05-17 11:47:47 -0500

    rm TODO

--- gcloudsetup.sh
+++ gcloudsetup.sh
@@ -5,20 +5,20 @@ else
 	for i in `seq 1 $1`; do
 		echo "INITIALIZING earth-$i..."
 		gcloud compute instances create earth-$i
-		gcloud compute scp ~/.ssh/google-cloud-cs123 earth-$i:~/.ssh/id_rsa --ssh-key-file=~/.ssh/google-cloud-cs123
 		gcloud compute scp ./chain.sh earth-$i:~/ --ssh-key-file=~/.ssh/google-cloud-cs123
 	done
 	gcloud compute instances list --format=text \
 	| grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g' > hosts
 	for i in `seq 1 $1`; do
+		gcloud compute scp ~/.ssh/google-cloud-cs123 earth-$i:~/.ssh/id_rsa --ssh-key-file=~/.ssh/google-cloud-cs123
 		gcloud compute scp hosts earth-$i:~/ --ssh-key-file=~/.ssh/google-cloud-cs123
 		gcloud compute ssh earth-$i --ssh-key-file=~/.ssh/google-cloud-cs123 --command="echo 'Installing dependencies on earth-$i...'; 
 		sudo apt-get -y install mpich; 
 		sudo apt-get -y install python-pip; 
 		sudo apt-get -y install python-dev; 
-		yes | sudo pip install mpi4py; 
 		sudo apt-get -y install python3-dev; 
 		sudo apt-get -y install python3-pip; 
+		yes | sudo pip install mpi4py; 
 		yes | sudo pip3 install mpi4py; 
 		yes | sudo pip install numpy; 
 		yes | sudo pip3 install numpy;
@@ -29,12 +29,5 @@ else
 	done
 fi
 
-# attempt to fix above
-# use ssh tool to connect each instances?
-# need to connect via internal IPs at the end
-
 # TO DELETE VM INSTANCES
 #gcloud compute instances delete my-instance --zone us-central1-a
-
-# TODO
-#
