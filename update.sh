4a08ebbdbc1361cfd13624ffe7dcd147c786907c
Jo Denby <jgdenby@uchicago.edu>
2018-05-21 10:45:31 -0500

    added documentation

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#FIRST ARG IS FILENAME, SECOND ARG IS NUM OF NODES
+# e.g., ./update.sh mpifile.py 3
+
 if [[  $# -ne 2 || $2 -le 0 ]]; then
 	echo "Incorrect input: Supply the file to transfer and the number of VM instances"
 else
