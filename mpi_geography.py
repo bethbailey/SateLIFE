97af14da576123ef25c965d914b18ada8ee0e0f9
Bethany Bailey <baileyb@uchicago.edu>
2018-05-20 19:55:35 -0500

    Adding difference between times for geography calculation)

---
+++ mpi_geography.py
@@ -0,0 +1,31 @@
+
+from mpi4py import MPI
+import numpy as np
+import skimage.external.tifffile as tiff
+
+comm = MPI.COMM_WORLD
+rank, size = comm.Get_rank(), comm.Get_size()
+
+# Needs to be two bands only.
+# Do a for loop over the bands we want?
+if rank == 0:
+    data = np.array([[[1,2],[1,2],[1,2]], [[1,3],[1,4],[1,5]]])
+    chunks = np.array_split(data, size)
+else:
+    chunks = None
+
+chunk = comm.scatter(chunks, root=0)
+
+results = []
+for x in chunk:
+    diff = np.diff(x)
+    results.append(diff)
+
+gathered_chunks = comm.gather(results, root=0)
+
+if rank == 0:
+    diff_array = gathered_chunks[0]
+    for i in range(len(gathered_chunks)):
+        if i != 0:
+            diff_array = np.concatenate((diff_array, gathered_chunks[i]))
+    print(diff_array)
