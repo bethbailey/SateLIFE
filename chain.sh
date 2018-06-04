7aebb116aaf1d89a719f3e682e12fc024bd8c8fe
Jo Denby <jgdenby@uchicago.edu>
2018-06-03 19:22:51 -0500

    added shell scripts documentation

--- chain.sh
+++ chain.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Script to automate nodes' SSH linking for MPI purposes
+# This script is called from within gcloudsetup.sh
+
 for i in $(cat hosts); do
 	ssh -oStrictHostKeyChecking=no $i "echo connected to $i"
 done
